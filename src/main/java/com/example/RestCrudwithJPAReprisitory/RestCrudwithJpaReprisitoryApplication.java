0944542b952ffa72a025a5a4e603817ffb971640
ekpei <anthony4arsenal@gmail.com>
2024-01-08 10:44:49 +0100

    Initial commit

---
+++ src/main/java/com/example/RestCrudwithJPAReprisitory/RestCrudwithJpaReprisitoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestCrudwithJPAReprisitory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudwithJpaReprisitoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudwithJpaReprisitoryApplication.class, args);
+	}
+
+}
